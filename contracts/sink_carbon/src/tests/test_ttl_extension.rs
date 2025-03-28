cab7164c71c331a36a9f980c40ad3e867a914b9a
Alex Olieman <alex@olieman.net>
2025-03-28 10:35:37 +0100

    test ttl extension for is_active

--- contracts/sink_carbon/src/tests/test_ttl_extension.rs
+++ contracts/sink_carbon/src/tests/test_ttl_extension.rs
@@ -19,7 +19,7 @@ fn test_extend_ttl_sink_carbon() {
         // is created the current ledger is counted towards the number of
         // ledgers specified by `min_persistent/temp_entry_ttl`, thus
         // the TTL is 1 ledger less than the respective setting.
-        assert_eq!(env.storage().instance().get_ttl(), 35_000);
+        assert_eq!(35_000, env.storage().instance().get_ttl());
         // TODO: extend with the other storage types if they are used in the future
         // assert_eq!(env.storage().persistent().get_ttl(&DataKey::MyKey), 499);
         // assert_eq!(env.storage().temporary().get_ttl(&DataKey::MyKey), 99);
@@ -52,6 +52,38 @@ fn test_extend_ttl_sink_carbon() {
 
     // sink more CARBON, expected TTL is 30 days again
     assert!(sink_carbon_with_auth(&setup, &test_data).is_ok());
+    env.as_contract(&setup.contract_id, || {
+        assert_eq!(30 * DAY_IN_LEDGERS, env.storage().instance().get_ttl());
+    });
+}
+
+#[test]
+fn test_extend_ttl_is_active() {
+    let setup = set_up_contracts_with_ttl_settings(60 * DAY_IN_LEDGERS);
+    let env = setup.env.clone();
+    let client = setup.sink_client;
+
+    env.as_contract(&setup.contract_id, || {
+        assert_eq!(35_000, env.storage().instance().get_ttl());
+    });
+    assert_eq!(true, client.is_active());
+
+    // expected TTL is 30 days
+    env.as_contract(&setup.contract_id, || {
+        assert_eq!(30 * DAY_IN_LEDGERS, env.storage().instance().get_ttl());
+    });
+
+    // Now bump the ledger sequence by 2 days in order to sanity-check
+    // the threshold settings of `extend_ttl` operations.
+    env.ledger().with_mut(|li| {
+        li.sequence_number = 100_000 + 2 * DAY_IN_LEDGERS;
+    });
+    env.as_contract(&setup.contract_id, || {
+        assert_eq!(28 * DAY_IN_LEDGERS, env.storage().instance().get_ttl());
+    });
+
+    // call is_active again, expected TTL is back to 30 days
+    assert_eq!(true, client.is_active());
     env.as_contract(&setup.contract_id, || {
         assert_eq!(30 * DAY_IN_LEDGERS, env.storage().instance().get_ttl());
     });
