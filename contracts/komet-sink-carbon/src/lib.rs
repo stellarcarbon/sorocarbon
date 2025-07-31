b2178b8d1657dd3463de50fab0cecb906f8da0ae
Alex Olieman <alex@olieman.net>
2025-07-31 16:56:04 +0200

    complete test_swap_is_atomic assertions

--- contracts/komet-sink-carbon/src/lib.rs
+++ contracts/komet-sink-carbon/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
 use soroban_sdk::{
-    contract, contractimpl, contracttype, 
-    token::StellarAssetClient, 
+    contract, contractimpl, contracttype,
+    token::{StellarAssetClient, TokenClient}, 
     Address, Bytes, Env, IntoVal, String, Symbol, Val, Vec
 };
 
@@ -65,16 +65,24 @@ impl TestSinkContract {
         recipient: Address, 
         amount: i64, 
         project_id: Symbol,
+        // TODO: String param, check blanket try...except
     ) -> bool {
         // bail if `amount` is not valid for mint
         // TODO: check on `assume` status in komet#74
-        if 1 > amount || amount > 2^60 {
+        if 1 > amount {
             return true;
         }
-        // credit the funder with exactly `amount` of CARBON
-        let csink_addr = env.storage().instance().get(&DataKey::CarbonSinkID).expect(
-            "CarbonSINK address must be set."
+        // create SAC token clients
+        let carbon_addr = env.storage().instance().get(&DataKey::CarbonID).expect(
+            "CARBON SAC address must be set."
         );
+        let csink_addr = env.storage().instance().get(&DataKey::CarbonSinkID).expect(
+            "CarbonSINK SAC address must be set."
+        );
+        let carbon_token_client = TokenClient::new(&env, &carbon_addr);
+        let csink_token_client = TokenClient::new(&env, &csink_addr);
+        
+        // credit the funder with exactly `amount` of CARBON
         let carbon_sac_client = StellarAssetClient::new(&env, &csink_addr);
         carbon_sac_client.mint(&funder, &amount.into());
 
@@ -82,13 +90,51 @@ impl TestSinkContract {
         let sink_addr: Address = env.storage().instance().get(&DataKey::SinkID).unwrap();
         let sink_client = sink_contract::Client::new(&env, &sink_addr);
 
-        // Call the `is_active` method of the sink contract
+        // collect balances before the swap
+        let contract_carbon_before = carbon_token_client.balance(&sink_addr);
+        let contract_csink_before = csink_token_client.balance(&sink_addr);
+        let funder_carbon_before = carbon_token_client.balance(&funder);
+        let funder_csink_before = csink_token_client.balance(&funder);
+        let recipient_carbon_before = carbon_token_client.balance(&recipient);
+        let recipient_csink_before = csink_token_client.balance(&recipient);
+
+        // Call the `sink_carbon` method of the sink contract
         let empty_string = String::from_str(&env, "");
         let sink_res = sink_client.try_sink_carbon(
             &funder, &recipient, &amount, &project_id, &empty_string, &empty_string
         );
         // TODO: check for SinkError::AmountTooLow
+
+        // collect balances after the swap
+        let contract_carbon_after = carbon_token_client.balance(&sink_addr);
+        let contract_csink_after = csink_token_client.balance(&sink_addr);
+        let funder_carbon_after = carbon_token_client.balance(&funder);
+        let funder_csink_after = csink_token_client.balance(&funder);
+        let recipient_carbon_after = carbon_token_client.balance(&recipient);
+        let recipient_csink_after = csink_token_client.balance(&recipient);        
+
+        // assert all contract balances are empty
+        let contract_balances = [
+            contract_carbon_before, contract_csink_before, 
+            contract_carbon_after, contract_csink_after
+        ];
+        let max_balance = contract_balances.iter().max().unwrap();
+        if *max_balance > 0 { return false; }
+
+        // assert the effect on funder balances
+        if funder_carbon_before != amount as i128 { return false; }
+        if funder_carbon_after >= 10_000 { return false; }
+        if funder_csink_before != 0 { return false; }
+        if funder_csink_after != 0 { return false; }
+
+        // assert the effect on recipient balances
+        if recipient_carbon_before != 0 { return false; }
+        if recipient_carbon_after != 0 { return false; }
+        if recipient_csink_before != 0 { return false; }
+        let quantization_remainder = amount as i128 - recipient_csink_after;
+        if quantization_remainder >= 10_000 { return false; }
       
-        true
+        // assert quantization remainders are equal
+        funder_carbon_after == quantization_remainder
     }
 }
